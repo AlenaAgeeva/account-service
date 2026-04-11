c117ba8a7e1c326e0105a27b5c3117ac6f09ae63
Alena Ageeva <alenaageevapv@gmail.com>
2026-04-11 14:25:22 -0300

    feat(TransactionRepository): add repository for Transaction entity.

--- src/main/java/com/ageeva/accountservice/repository/TransactionRepository.java
+++ src/main/java/com/ageeva/accountservice/repository/TransactionRepository.java
@@ -1,2 +1,26 @@
-package com.ageeva.accountservice.repository;public interface TransactionRepository {
+package com.ageeva.accountservice.repository;
+
+import com.ageeva.accountservice.entity.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findByAccountIdOrderByCreatedAtDesc(UUID accountId);
+
+    @Query("SELECT t FROM Transaction t WHERE t.accountId = :accountId AND t.createdAt BETWEEN :start " +
+            "AND :end ORDER BY t.createdAt DESC")
+    List<Transaction> findByAccountIdAndDateRange(
+            @Param("accountId") UUID accountId,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end
+    );
+
+    boolean existsByTransactionReference(String transactionReference);
+
+    List<Transaction> findByRelatedTransactionId(UUID relatedTransactionId);
 }
