ad6e8a0ffe713e250c2e2b49656963412966a219
Alena Ageeva <alenaageevapv@gmail.com>
2026-04-17 16:17:17 -0300

    refactor(TransactionRepository): refactor TransationRepository.

--- src/main/java/com/ageeva/accountservice/repository/TransactionRepository.java
+++ src/main/java/com/ageeva/accountservice/repository/TransactionRepository.java
@@ -1,16 +1,20 @@
 package com.ageeva.accountservice.repository;
 
 import com.ageeva.accountservice.entity.transaction.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
-    List<Transaction> findByAccountIdOrderByCreatedAtDesc(UUID accountId);
+    Page<Transaction> findByAccountIdOrderByCreatedAtDesc(UUID accountId, Pageable pageable);
 
     @Query("SELECT t FROM Transaction t WHERE t.accountId = :accountId AND t.createdAt BETWEEN :start " +
             "AND :end ORDER BY t.createdAt DESC")
@@ -23,4 +27,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, UUID> 
     boolean existsByTransactionReference(String transactionReference);
 
     List<Transaction> findByRelatedTransactionId(UUID relatedTransactionId);
+
+    Optional<Transaction> findByTransactionReference(String transactionReference);
 }
